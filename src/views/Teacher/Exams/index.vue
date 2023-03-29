9ff9b4781386f0274633fa8756463179df27ca9f
Ibrahim Sherif <ibrahimsherif223@gmail.com>
2023-03-29 04:50:02 +0100

    IMS 70 percent done

--- src/views/Teacher/Exams/index.vue
+++ src/views/Teacher/Exams/index.vue
@@ -1,52 +1,158 @@
 <template>
   <CContainer>
-    <CCard>
-      <CCardHeader>Questions</CCardHeader>
+    <CCard class="mb-3 border-light border-top-primary border-top-3">
+      <CCardHeader class="bg-white">
+        <CRow>
+          <CCol sm="9" class="d-flex align-items-center">
+            <h5>Questions</h5>
+          </CCol>
+          <CCol sm="3" class="d-sm-flex justify-content-sm-end">
+            <CButton
+              color="primary"
+              @click="
+                () => {
+                  questionModal = true
+                }
+              "
+              ><span class="text-white">Add Question</span></CButton
+            >
+          </CCol>
+        </CRow>
+      </CCardHeader>
+      <CCardBody>
+        <CTable>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col">#</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Type</CTableHeaderCell>
+              <CTableHeaderCell scope="col">
+                Number of questions
+              </CTableHeaderCell>
+              <CTableHeaderCell scope="col" class="w-25">
+                Action
+              </CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableHeaderCell scope="row">1</CTableHeaderCell>
+              <CTableDataCell>MCQs</CTableDataCell>
+              <CTableDataCell>2</CTableDataCell>
+              <CTableDataCell>
+                <CButton color="info" size="sm" variant="outline">
+                  View All
+                </CButton>
+              </CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+      </CCardBody>
+    </CCard>
+    <CCard class="mb-3 border-light border-top-primary border-top-3">
+      <CCardHeader lass="bg-white d-flex align-items-center">
+        <h5>Exams</h5>
+      </CCardHeader>
       <CCardBody>
         <CRow>
-          <CCol md="4">
-            <CCard>
-              <CCardHeader>ADD QUESTION</CCardHeader>
+          <CCol md="12">
+            <CCard class="border-light border-top-warning border-top-2">
+              <CCardHeader class="text-center text-bold">
+                CREATE AN EXAM
+              </CCardHeader>
               <CCardBody>
                 <CForm>
-                  <CFormInput
-                    type="email"
-                    id="exampleFormControlInput1"
-                    label="Email address"
-                    placeholder="name@example.com"
-                    text="Must be 8-20 characters long."
-                  />
+                  <CRow>
+                    <CCol md="6">
+                      <CInputGroup class="mb-3">
+                        <CInputGroupText id="basic-addon1">#</CInputGroupText>
+                        <CFormInput
+                          type="text"
+                          placeholder="Name"
+                          aria-label="name"
+                          aria-describedby="basic-addon1"
+                        />
+                      </CInputGroup>
+                    </CCol>
+                    <CCol md="6">
+                      <CInputGroup>
+                        <CInputGroupText>
+                          <i class="bi bi-book"></i>
+                        </CInputGroupText>
+                        <CFormSelect>
+                          <option>Select Course</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                        </CFormSelect>
+                      </CInputGroup>
+                    </CCol>
+                    <CCol md="6">
+                      <CInputGroup class="mb-3">
+                        <CInputGroupText id="basic-addon2">
+                          <i class="bi bi-calendar2"></i>
+                        </CInputGroupText>
+                        <CFormInput type="date" />
+                      </CInputGroup>
+                    </CCol>
+                    <CCol md="6">
+                      <CInputGroup class="mb-3">
+                        <CInputGroupText id="basic-addon3">
+                          <i class="bi bi-clock"></i>
+                        </CInputGroupText>
+                        <CFormInput
+                          type="text"
+                          placeholder="Duration (in hours)"
+                          aria-label="duration"
+                          aria-describedby="basic-addon3"
+                        />
+                      </CInputGroup>
+                    </CCol>
+                  </CRow>
                 </CForm>
               </CCardBody>
             </CCard>
           </CCol>
-          <CCol md="8">
+          <CCol md="12">
             <CTable>
               <CTableHead>
                 <CTableRow>
                   <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Date</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Duration</CTableHeaderCell>
+                  <CTableHeaderCell scope="col"
+                    >No of questions</CTableHeaderCell
+                  >
+                  <CTableHeaderCell scope="col">Action</CTableHeaderCell>
                 </CTableRow>
               </CTableHead>
               <CTableBody>
                 <CTableRow>
                   <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>@mdo</CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">2</CTableHeaderCell>
-                  <CTableDataCell>Jacob</CTableDataCell>
-                  <CTableDataCell>Thornton</CTableDataCell>
-                  <CTableDataCell>@fat</CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">3</CTableHeaderCell>
-                  <CTableDataCell colspan="2">Larry the Bird</CTableDataCell>
-                  <CTableDataCell>@twitter</CTableDataCell>
+                  <CTableDataCell>First Term Exam</CTableDataCell>
+                  <CTableDataCell>19-02-2020</CTableDataCell>
+                  <CTableDataCell>2hrs</CTableDataCell>
+                  <CTableDataCell>30</CTableDataCell>
+                  <CTableDataCell>
+                    <CButton
+                      color="info"
+                      variant="outline"
+                      size="sm"
+                      class="me-2"
+                    >
+                      View
+                    </CButton>
+                    <CButton
+                      color="warning"
+                      variant="outline"
+                      size="sm"
+                      class="me-2"
+                    >
+                      Edit
+                    </CButton>
+                    <CButton color="danger" variant="outline" size="sm">
+                      Delete
+                    </CButton>
+                  </CTableDataCell>
                 </CTableRow>
               </CTableBody>
             </CTable>
@@ -54,60 +160,127 @@
         </CRow>
       </CCardBody>
     </CCard>
-    <CCard>
-      <CCardHeader>Exams</CCardHeader>
-      <CCardBody>
-        <CRow>
-          <CCol md="4">
-            <CCard>
-              <CCardHeader>ADD AN EXAM</CCardHeader>
-              <CCardBody>
-                <CForm>
-                  <CFormInput
-                    type="email"
-                    id="exampleFormControlInput1"
-                    label="Email address"
-                    placeholder="name@example.com"
-                    text="Must be 8-20 characters long."
-                  />
-                </CForm>
+    <CModal
+      size="lg"
+      backdrop="static"
+      :visible="questionModal"
+      @close="
+        () => {
+          questionModal = false
+        }
+      "
+    >
+      <CModalHeader>
+        <CModalTitle>Add New Question</CModalTitle>
+      </CModalHeader>
+      <CModalBody>
+        <CForm>
+          <div class="form-group mb-2">
+            <CRow>
+              <CCol sm="2">
+                <CFormLabel for="questionText">Structure</CFormLabel>
+              </CCol>
+              <CCol sm="10">
+                <CFormTextarea
+                  id="questionText"
+                  rows="3"
+                  placeholder="Enter question text"
+                ></CFormTextarea>
+              </CCol>
+            </CRow>
+          </div>
+          <div class="form-group mb-2">
+            <CRow>
+              <CCol sm="2">
+                <CFormLabel for="questionType">Type</CFormLabel>
+              </CCol>
+              <CCol sm="10">
+                <CFormSelect @change="onChoice($event)">
+                  <option value="0">Choose type</option>
+                  <option value="1">MCQ</option>
+                  <option value="2">Structural</option>
+                </CFormSelect>
+              </CCol>
+            </CRow>
+          </div>
+          <div v-if="questionType == 1" class="mb-2">
+            <CCard class="border-light">
+              <CCardBody class="text-center">
+                <CCardTitle>MCQs</CCardTitle>
+                <p><small>Tick the correct answer</small></p>
+                <CRow class="g-2">
+                  <CCol md="6">
+                    <CInputGroup>
+                      <CInputGroupText> 1: </CInputGroupText>
+                      <CFormTextarea
+                        id="questionChoice1"
+                        rows="3"
+                        placeholder="Enter first option"
+                      ></CFormTextarea>
+                    </CInputGroup>
+                    <CFormCheck id="questionCheck1" />
+                  </CCol>
+                  <CCol md="6">
+                    <CInputGroup>
+                      <CInputGroupText> 2: </CInputGroupText>
+                      <CFormTextarea
+                        id="questionChoice2"
+                        rows="3"
+                        placeholder="Enter second option"
+                      ></CFormTextarea>
+                    </CInputGroup>
+                    <CFormCheck id="questionCheck2" />
+                  </CCol>
+                  <CCol md="6">
+                    <CInputGroup>
+                      <CInputGroupText> 3: </CInputGroupText>
+                      <CFormTextarea
+                        id="questionChoice3"
+                        rows="3"
+                        placeholder="Enter third option"
+                      ></CFormTextarea>
+                    </CInputGroup>
+                    <CFormCheck id="questionCheck3" />
+                  </CCol>
+                  <CCol md="6">
+                    <CInputGroup>
+                      <CInputGroupText> 4: </CInputGroupText>
+                      <CFormTextarea
+                        id="questionChoice4"
+                        rows="3"
+                        placeholder="Enter fourth option"
+                      ></CFormTextarea>
+                    </CInputGroup>
+                    <CFormCheck id="questionCheck4" />
+                  </CCol>
+                </CRow>
               </CCardBody>
             </CCard>
-          </CCol>
-          <CCol md="8">
-            <CTable>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>@mdo</CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">2</CTableHeaderCell>
-                  <CTableDataCell>Jacob</CTableDataCell>
-                  <CTableDataCell>Thornton</CTableDataCell>
-                  <CTableDataCell>@fat</CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">3</CTableHeaderCell>
-                  <CTableDataCell colspan="2">Larry the Bird</CTableDataCell>
-                  <CTableDataCell>@twitter</CTableDataCell>
-                </CTableRow>
-              </CTableBody>
-            </CTable>
-          </CCol>
-        </CRow>
-      </CCardBody>
-    </CCard>
+          </div>
+          <div v-if="questionType == 2" class="form-group mb-2">
+            <CCard class="border-light">
+              <CCardBody>
+                <CCardTitle class="text-center">Structural</CCardTitle>
+                <CRow> </CRow>
+              </CCardBody>
+            </CCard>
+          </div>
+        </CForm>
+      </CModalBody>
+      <CModalFooter>
+        <CButton
+          color="secondary"
+          @click="
+            () => {
+              questionModal = false
+            }
+          "
+        >
+          Close
+        </CButton>
+        <CButton color="primary">Save</CButton>
+      </CModalFooter>
+    </CModal>
   </CContainer>
 </template>
 
@@ -119,9 +292,17 @@ import { dateFormat } from '@/utils/dateFormat.js'
 export default {
   name: 'index',
   data() {
-    return {}
+    return {
+      questionModal: false,
+      questionType: 0,
+    }
   },
-  components: {},
+  methods: {
+    onChoice(event) {
+      this.questionType = event.target.value
+      console.log(event.target.value)
+    },
+  },
   setup() {
     const { error, exams, load } = getExam()
     const tabPanePillsActiveKey = ref(1)
